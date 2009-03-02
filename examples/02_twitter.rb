8d3ec8df0c35f3dde2aaca19b17592d2527b16a9
ucnv <ucnvvv@gmail.com>
2009-03-02 14:27:34 +0900

    change method

--- examples/02_twitter.rb
+++ examples/02_twitter.rb
@@ -1,15 +1,15 @@
-require 'rubygems'
+﻿require 'rubygems'
 require 'chainsaw'
 
 username, password, tweet = ARGV
 
-Chainsaw.launch('https://twitter.com/home').> { |a|
+Chainsaw.launch('https://twitter.com/home').process { |a|
   f = a.doc.css('.signin').first
   f.xpath('id("username_or_email")').first['value'] = username
   f.xpath('id("session[password]")').first['value'] = password
   a.set_next f
-}.> { |a|
+}.process { |a|
   f = a.doc.css('#doingForm').first
   f.xpath('id("status")').first.content = tweet || "(o'-')-o fwip fwip"
   a.set_next f
-}.>
+}.process
